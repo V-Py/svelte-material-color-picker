68c43be7c5c08dfd55a51611bbd86448336a59db
Valentin Py <valentin.pyb@gmail.com>
2022-02-23 08:16:47 +0100

    First commit

---
+++ src/lib/components/ColorCircle.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let color = "#e91e63";
+    export let size = 50;
+    export let margin = 5;
+</script>
+
+
+<div class="circle" style:background-color="{color}" style:height="{size}px" style:width="{size}px" style:margin="{margin}px"></div>
+
+
+<style>
+
+    .circle{
+        border-radius:50%;
+        border:none;
+    }
+
+    .circle:hover{
+        cursor:pointer;
+        filter: brightness(0.8);
+    }
+</style>
